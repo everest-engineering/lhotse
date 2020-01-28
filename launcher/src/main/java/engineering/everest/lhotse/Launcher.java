c8e43a27b9829c1ee303e4b691b99e38abf4d284
Venkata Jaswanth U <j@everest.engineering>
2020-01-28 13:09:34 +0530

    Explicity specify repository annotation packages to allow spring to instantiate them since the file-storage libray uses the same annotation to configure it's internal repositories

--- launcher/src/main/java/engineering/everest/lhotse/Launcher.java
+++ launcher/src/main/java/engineering/everest/lhotse/Launcher.java
@@ -3,10 +3,12 @@ package engineering.everest.lhotse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan("engineering.everest.lhotse")
+@EnableMongoRepositories("engineering.everest.lhotse")
 @EnableScheduling
 @SuppressWarnings("PMD.ClassWithOnlyPrivateConstructorsShouldBeFinal")
 public class Launcher {
