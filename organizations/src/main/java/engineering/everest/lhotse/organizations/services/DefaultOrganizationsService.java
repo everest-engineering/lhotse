cb30a0a66a0326b92a871a7b535a7f7e635c3c87
Sebastian Luehr <sebastian.luehr@everest.engineering>
2020-03-12 14:59:04 +1100

    Fixed broken command dispatching

--- organizations/src/main/java/engineering/everest/lhotse/organizations/services/DefaultOrganizationsService.java
+++ organizations/src/main/java/engineering/everest/lhotse/organizations/services/DefaultOrganizationsService.java
@@ -1,12 +1,12 @@
 package engineering.everest.lhotse.organizations.services;
 
 
+import engineering.everest.lhotse.axon.common.RandomFieldsGenerator;
 import engineering.everest.lhotse.organizations.domain.commands.DeregisterOrganizationCommand;
 import engineering.everest.lhotse.organizations.domain.commands.RegisterOrganizationCommand;
 import engineering.everest.lhotse.organizations.domain.commands.ReregisterOrganizationCommand;
 import engineering.everest.lhotse.organizations.domain.commands.UpdateOrganizationCommand;
 import engineering.everest.starterkit.axon.HazelcastCommandGateway;
-import engineering.everest.lhotse.axon.common.RandomFieldsGenerator;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
