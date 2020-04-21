8b9517be76c12f0441f1b8649dd7811e961775d8
Yang Wang <ywangd@gmail.com>
2020-04-21 21:45:08 +1000

    For test coverage

---
+++ axon-support/src/test/java/engineering/everest/lhotse/axon/replay/EverestTrackingEventProcessorTest.java
@@ -0,0 +1,74 @@
+package engineering.everest.lhotse.axon.replay;
+
+import org.axonframework.common.transaction.TransactionManager;
+import org.axonframework.eventhandling.TrackedEventMessage;
+import org.axonframework.eventhandling.TrackingEventProcessor;
+import org.axonframework.eventhandling.TrackingToken;
+import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.messaging.StreamableMessageSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EverestTrackingEventProcessorTest {
+
+    private final String PROCESSOR_NAME = "default";
+
+    @Mock
+    private TrackingEventProcessor trackingEventProcessor;
+    @Mock
+    private TransactionManager transactionManager;
+    @Mock
+    private TokenStore tokenStore;
+    @Mock
+    private StreamableMessageSource<? extends TrackedEventMessage<?>> streamableMessageSource;
+    @Mock
+    private TrackingToken headToken;
+    @Mock
+    private TrackingToken startPosition;
+
+    private EverestTrackingEventProcessor everestTrackingEventProcessor;
+
+    @BeforeEach
+    void setUp() {
+        everestTrackingEventProcessor = new EverestTrackingEventProcessor(
+                trackingEventProcessor,
+                transactionManager,
+                tokenStore,
+                1
+        );
+        when(everestTrackingEventProcessor.getName()).thenReturn(PROCESSOR_NAME);
+        Mockito.<StreamableMessageSource<? extends TrackedEventMessage<?>>>when(
+                everestTrackingEventProcessor.getMessageSource()).thenReturn(streamableMessageSource);
+        when(streamableMessageSource.createHeadToken()).thenReturn(headToken);
+        doAnswer(invocation -> {
+            ((Runnable) invocation.getArgument(0)).run();
+            return null;
+        }).when(transactionManager).executeInTransaction(isA(Runnable.class));
+    }
+
+    @Test
+    void resetTokensWillInitialiseSegmentsAndDelegate() {
+        when(tokenStore.fetchSegments(PROCESSOR_NAME)).thenReturn(new int[0]);
+        everestTrackingEventProcessor.resetTokens(startPosition);
+        verify(tokenStore).initializeTokenSegments(PROCESSOR_NAME, 1, headToken);
+        verify(trackingEventProcessor).resetTokens(startPosition);
+    }
+
+    @Test
+    void resetTokensWillStoreHeadTokenAndDelegate() {
+        when(tokenStore.fetchSegments(PROCESSOR_NAME)).thenReturn(new int[]{0});
+        everestTrackingEventProcessor.resetTokens(startPosition);
+        verify(tokenStore).storeToken(headToken, PROCESSOR_NAME, 0);
+        verify(trackingEventProcessor).resetTokens(startPosition);
+    }
+}
