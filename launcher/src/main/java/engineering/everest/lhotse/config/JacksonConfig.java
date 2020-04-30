1ad344afe3c6f8d2ea80851d107c7ca6c22cc50b
Sebastian Luehr <sebastian.luehr@everest.engineering>
2020-04-30 16:04:45 +1000

    Lhotse#6: removed unused dependencies and marked 'api' dependencies that we leak

--- launcher/src/main/java/engineering/everest/lhotse/config/JacksonConfig.java
+++ launcher/src/main/java/engineering/everest/lhotse/config/JacksonConfig.java
@@ -1,4 +1,4 @@
-package engineering.everest.lhotse.axon.config;
+package engineering.everest.lhotse.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
