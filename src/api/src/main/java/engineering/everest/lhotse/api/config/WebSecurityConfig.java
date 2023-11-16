77db6e9178ff75d3d8fb4196d0e560e37d1604f9
Prasanna <lakshmiprasanna@everest.engineering>
2023-11-16 14:32:56 +0530

    Remove depricated spring security methods

--- src/api/src/main/java/engineering/everest/lhotse/api/config/WebSecurityConfig.java
+++ src/api/src/main/java/engineering/everest/lhotse/api/config/WebSecurityConfig.java
@@ -6,12 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -29,23 +32,24 @@ public class WebSecurityConfig {
         var jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(keycloakJwtGrantedAuthoritiesConverter);
 
-        http.cors().and().csrf().disable()
-            .authorizeHttpRequests()
-            .requestMatchers(
-                "/api/organizations/**",
-                "/api/version",
-                "/actuator/health",
-                "/api/doc/**",
-                "/swagger-ui/**",
-                "/swagger-resources/**",
-                "/sso/login*")
-            .permitAll()
-            .requestMatchers("/api/**").authenticated()
-            .requestMatchers("/actuator/health/**", "/actuator/metrics/**", "/actuator/prometheus").hasAnyRole("ADMIN", "MONITORING")
-            .requestMatchers("/actuator/**", "/admin/**").hasRole("ADMIN")
-            .anyRequest().permitAll()
-            .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")).logoutSuccessUrl("/")
-            .and().oauth2ResourceServer(configurer -> configurer.jwt().jwtAuthenticationConverter(jwtAuthenticationConverter));
+        http.cors(withDefaults()).csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers(
+                    "/api/organizations/**",
+                    "/api/version",
+                    "/actuator/health",
+                    "/api/doc/**",
+                    "/swagger-ui/**",
+                    "/swagger-resources/**",
+                    "/sso/login*")
+                .permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .requestMatchers("/actuator/health/**", "/actuator/metrics/**", "/actuator/prometheus").hasAnyRole("ADMIN", "MONITORING")
+                .requestMatchers("/actuator/**", "/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll())
+            .logout((logout) -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")).logoutSuccessUrl("/"))
+            .oauth2ResourceServer(
+                oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
         return http.build();
     }
 }
